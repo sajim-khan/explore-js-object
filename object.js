0919103f23ebbe9891adf3166a07873a72f52207
sajim-khan <shossain1427@gmail.com>
2023-02-28 01:16:08 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,12 @@
+// create object using object literal
+
+const player = {};
+player.name = 'Sajim'
+player.speciality = 'Honest';
+player.bat = function () { 
+    console.log('Swing your bat');
+}
+
+console.log(player);
+
+player.bat();
