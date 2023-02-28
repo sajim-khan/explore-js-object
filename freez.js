1a719d7294ec42d5d4d5cc2019f750005621c4e6
sajim-khan <shossain1427@gmail.com>
2023-02-28 14:21:45 +0600

    added Keys, values, entries, delete

---
+++ freez.js
@@ -0,0 +1,14 @@
+const bottle = { color: 'blue', price: '435', isCleaned : true }
+const keys = Object.keys(bottle) // [ 'color', 'price', 'isCleaned' ]
+console.log(keys);
+const values = Object.values(bottle)
+console.log(values); // [ 'blue', '435', true ]
+
+const pair = Object.entries(bottle);
+console.log(pair);  // output: [ [ 'color', 'blue' ], [ 'price', '435' ], [ 'isCleaned', true ] ]
+
+console.log(bottle);  // output: { color: 'blue', price: '435', isCleaned: true }
+delete bottle.isCleaned
+console.log(bottle); // output: { color: 'blue', price: '435' }
+
+// add property
